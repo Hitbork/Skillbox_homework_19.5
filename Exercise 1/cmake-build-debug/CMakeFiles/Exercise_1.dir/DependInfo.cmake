21cedbf1ae59822a61fd64730123ea7bf63b9bb1
Hitbork <hitbork@yandex.ru>
2023-09-15 11:17:12 +0300

    Exercise 1 - 1.0.0

---
+++ Exercise 1/cmake-build-debug/CMakeFiles/Exercise_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Skillbox_repositories/Skillbox_homework_19.5/Exercise 1/main.cpp" "CMakeFiles/Exercise_1.dir/main.cpp.obj" "gcc" "CMakeFiles/Exercise_1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Exercise 1/main.cpp
+++ Exercise 1/main.cpp
@@ -8,17 +8,26 @@ int main() {
 
     // Creating vars
     int counter = 0;
-    std::string word = "so", temp;
+    std::string word = "so", wordWithDifferentStartLetter, temp;
+
+    // Changing 1st letter for the word to search
+    if ((int)word[0] <= 90) {
+        // Changing 1st letter to lower
+        wordWithDifferentStartLetter = (char)tolower(word[0]) + word.substr(1, word.length()-1);
+    } else {
+        // Changing 1st letter to upper
+        wordWithDifferentStartLetter = (char)toupper(word[0]) + word.substr(1, word.length()-1);
+    }
 
     // Opening file
-    std::ifstream words("C:\\Skillbox_github\\Homework 19.5\\Skillbox_homework_19.5\\Exercise 1\\words.txt");
+    std::ifstream words("C:\\Skillbox_repositories\\Skillbox_homework_19.5\\Exercise 1\\words.txt");
 
     if (!words.is_open()) {
         std::cout << "error\n";
     } else {
         while (!words.eof()) {
             words >> temp;
-            if (temp == word)
+            if ((temp == word) || (temp == wordWithDifferentStartLetter))
                 counter++;
         }
     }
